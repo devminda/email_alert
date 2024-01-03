04213bbebd51176620b8e41b2051bc91700461be
Devminda Lakshitha Abeynayake <devmindaabey@gmail.com>
2024-01-03 16:01:55 +0530

    added more to the emaillist

--- data/email_list.py
+++ data/email_list.py
@@ -1,2 +1,2 @@
-email_list = ['hmoemailtester@gmail.com', 'devmindaabey@gmail.com'] 
-            #   'devmindaabey@gmail.com', 'nlasini07@gmail.com', 'asithan476@gmail.com ']
+email_list = ['hmoemailtester@gmail.com', #'devmindaabey@gmail.com'] 
+              'devmindaabey@gmail.com', 'nlasini07@gmail.com', 'asithan476@gmail.com ']
